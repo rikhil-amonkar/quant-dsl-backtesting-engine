40328902f8bfa76f0ce4acd437deb72348754849
Rikhil Amonkar <rikhilma@gmail.com>
2026-04-25 22:37:45 -0400

    add dashed indication line to represent initial capital and combine line legends on display

--- graph.py
+++ graph.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from pandas import col
 
 # plot pnl results and market prices to graph visual
 def plot_pnl_values_to_graph(running_capital, daily_hash_update) -> None:
     
     daily_info_dump_parsed = [open_val for open_val in list(daily_hash_update.values())]  # open val
+    initial_capital = [running_capital[0]] * len(running_capital)
     
     # ohlcv breakdown --> volume too large
     day_open_prices = [val[0] for val in daily_info_dump_parsed]
@@ -13,16 +15,16 @@ def plot_pnl_values_to_graph(running_capital, daily_hash_update) -> None:
     day_close_prices = [val[3] for val in daily_info_dump_parsed]
             
     x_vals = [(n + 1) for n in range(0, len(day_open_prices))]  # days
-    # print(len(day_open_prices), len(x_vals), len(y_vals))
 
-    fig, ax1 = plt.subplots(figsize=(14, 6))  # width/height
-    ax1.plot(x_vals, day_open_prices, label="Open Price")
-    ax1.plot(x_vals, day_close_prices, label="Close Price")
-    ax1.plot(x_vals, day_high_prices, label="High Price")
-    ax1.plot(x_vals, day_low_prices, label="Low Price")
+    fig, ax1 = plt.subplots(figsize=(14, 8))  # width/height
+    open_line = ax1.plot(x_vals, day_open_prices, label="Open Price", color="#1f77b4")
+    close_line = ax1.plot(x_vals, day_close_prices, label="Close Price", color="#ff7f0e")
+    high_line = ax1.plot(x_vals, day_high_prices, label="High Price", color="#2ca02c")
+    low_line = ax1.plot(x_vals, day_low_prices, label="Low Price", color="#d62728")
         
     ax2 = ax1.twinx()
-    ax2.plot(x_vals, running_capital, label="Current Capital", drawstyle="steps-post")  # hold val till next point
+    init_cap_line = ax2.plot(x_vals, initial_capital, linestyle=":", label="Initial Capital", color="grey")  # starting cap (dotted)
+    curr_cap_line = ax2.plot(x_vals, running_capital, label="Current Capital", drawstyle="steps-post", color="black")  # hold val till next point
     
     # set axis titles
     ax1.set_ylabel("Price ($)")
@@ -31,7 +33,7 @@ def plot_pnl_values_to_graph(running_capital, daily_hash_update) -> None:
     # scale y-axis steps
     ax2.yaxis.set_major_locator(MaxNLocator(nbins=10))
     ax1.yaxis.set_major_locator(MaxNLocator(nbins=10))
-    
+        
     # track indexes of capital changes
     change_indices = []
     for i in range(1, len(running_capital)):
@@ -55,8 +57,14 @@ def plot_pnl_values_to_graph(running_capital, daily_hash_update) -> None:
             color=color,
             alpha=0.3
         )
-        
-    ax1.set_title("Simulated Capital Loss/Gain Over Market Data Period")
+
+    ax1.set_title("Portfolio Value Over Simulated Backtest Period")
     ax1.set_xlabel("Days")
     
+    all_lines = open_line + close_line + high_line + low_line + init_cap_line + curr_cap_line  # combine
+    all_labels = [line.get_label() for line in all_lines]  # extract labels
+    
+    ax1.legend(all_lines, all_labels, loc=4)
+    
+    plt.savefig("simulated_backtesting_pnl_graph.png")
     plt.show()
